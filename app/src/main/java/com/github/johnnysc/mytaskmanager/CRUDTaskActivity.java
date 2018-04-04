1614f3e70958dc611100d60b7b6cd0d80fc099c4
Asatryan Hovhannes <OSAsatryan.SBT@sberbank.ru>
2018-04-04 13:17:16 +0300

    optimize imports

--- app/src/main/java/com/github/johnnysc/mytaskmanager/CRUDTaskActivity.java
+++ app/src/main/java/com/github/johnnysc/mytaskmanager/CRUDTaskActivity.java
@@ -2,8 +2,6 @@ package com.github.johnnysc.mytaskmanager;
 
 import android.app.AlarmManager;
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
